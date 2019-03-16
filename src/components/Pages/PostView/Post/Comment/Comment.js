c852052d9ab1330710df1591f6e19e3fd157f0cb
Vahagn Khorotyan <khorotyanvahagn@gmail.com>
2019-03-16 22:16:19 +0400

    Ability to highlight and like post, add and like comments

--- src/components/Pages/PostView/Post/Comment/Comment.js
+++ src/components/Pages/PostView/Post/Comment/Comment.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IconButton, Typography } from '@material-ui/core';
 import FavoriteIcon from '@material-ui/icons/FavoriteBorder';
+import FavoriteFilledIcon from '@material-ui/icons/Favorite';
 import classes from './Comment.module.css';
 
+const LIKE_FILLED_COLOR = "#e10050";
+
 const comment = props => {
+
+    const [liked, setLike] = useState(false);
+
+    const handleLike = () => {
+        setLike(!liked);
+    };
+
     return (
         <div className={classes.commentParent}>
             <div className={classes.commentTextRoot}>
-                <Typography style={{paddingBottom: 8}} component="p">
+                <Typography style={{paddingBottom: 8, whiteSpace: 'pre-line'}} component="p">
                     <strong>zyuser</strong> {props.text}
                 </Typography>
             </div>
             <div className={classes.likeRoot}>
-                <IconButton className={classes.favoriteIconButton}>
-                    <FavoriteIcon style={{width: 18, height: 18}}/>
+                <IconButton onClick={handleLike} className={classes.favoriteIconButton}>
+                    {liked ? <FavoriteFilledIcon style={{color: LIKE_FILLED_COLOR, width: 18, height: 18}}/> : <FavoriteIcon style={{width: 18, height: 18}}/>}
                 </IconButton>
             </div>
         </div>
--- src/components/Pages/PostView/Post/Post.js
+++ src/components/Pages/PostView/Post/Post.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Link, BrowserRouter } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Link, BrowserRouter } from 'react-router-dom';
 import Comment from './Comment/Comment';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
@@ -12,13 +12,70 @@ import InputBase from '@material-ui/core/InputBase';
 import { IconButton, Typography } from '@material-ui/core';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import FavoriteIcon from '@material-ui/icons/FavoriteBorder';
+import FavoriteFilledIcon from '@material-ui/icons/Favorite';
 import ShareIcon from '@material-ui/icons/Share';
-import SaveIcon from '@material-ui/icons/BookmarkBorder';
+import BookmarkIcon from '@material-ui/icons/BookmarkBorder';
+import BookmarkFilledIcon from '@material-ui/icons/Bookmark';
 import classes from './Post.module.css';
 
 const AVATAR_BACKGROUND_COLOR = "#f44336";
+const LIKE_FILLED_COLOR = "#e10050";
 
 const post = props => {
+
+    const [liked, setLike] = useState(false);
+    const [bookmarked, setBookmark] = useState(false);
+    const [commentInputText, setCommentInputText] = useState("");
+    const [comments, setComments] = useState([]);
+
+    const handleLike = () => {
+        setLike(!liked);
+    };
+
+    const handleBookmark = () => {
+        setBookmark(!bookmarked);
+    }
+
+    const addComment = (text) => {
+        let id = 0;
+        text = text.trim();
+
+        if (comments.length !== 0) {
+            id = comments[0].id + 1;
+        }
+
+        const newComment = {
+            id: id,
+            text: text
+        };
+
+        const newComments = [...comments];
+        newComments.unshift(newComment)
+        
+        setCommentInputText("");
+        setComments(newComments);
+    } 
+
+    const handleAddCommentKeyPress = event => {
+        if (event.key === 'Enter') {
+            let commentInputTextCopy = commentInputText;
+
+            if (event.shiftKey) {
+
+            } else {
+                
+                
+                if (commentInputTextCopy.length > 0) {
+                    addComment(commentInputTextCopy);
+                } 
+            }   
+        }
+    }
+
+    const handleCommentInputChange = event => {
+        setCommentInputText(event.target.value.trimStart());
+    }
+
     return (
         <Card className={classes.card}>
             <CardHeader
@@ -41,14 +98,14 @@ const post = props => {
                 image="https://instagram.fevn1-1.fna.fbcdn.net/vp/8a77982ee081608b674889fe1cfe7edb/5D02176C/t51.2885-15/e35/53011206_366848223904033_748117012698865564_n.jpg?_nc_ht=instagram.fevn1-1.fna.fbcdn.net"
             />
             <CardActions style={{paddingBottom: 0}} disableActionSpacing>
-                <IconButton aria-label="Like">
-                    <FavoriteIcon/>
+                <IconButton onClick={handleLike} aria-label={liked ? "Unlike" : "Like"}>
+                    {liked ? <FavoriteFilledIcon style={{color: LIKE_FILLED_COLOR}}/> : <FavoriteIcon/>}
                 </IconButton>
                 <IconButton aria-label="Share">
                     <ShareIcon/>
                 </IconButton>
-                <IconButton style={{marginLeft: "auto"}}>
-                    <SaveIcon/>
+                <IconButton onClick={handleBookmark} style={{marginLeft: "auto"}}>
+                    {bookmarked ? <BookmarkFilledIcon/> : <BookmarkIcon/>}
                 </IconButton>
             </CardActions>
             <CardContent style={{paddingTop: 0}}>
@@ -56,10 +113,20 @@ const post = props => {
                     Liked by <strong>James Brown</strong> and <strong>11,650 others</strong>
                 </Typography>   
                 <Typography style={{paddingBottom: 12}} component="p">
-                    Heading back to India tomorrow after 3 years! So excited to explore Delhi, Agra and more for the next week! So much new content to come! in ALSO...! For those of you in Delhi I’m hosting a meet-up with <BrowserRouter><Link to="/user/:id" className={classes.userMention}>@jordhammond</Link></BrowserRouter> at 2pm on 18th March at India Gate - can’t wait to see you there!
+                    {props.description} 
+                    <BrowserRouter>
+                        <Link 
+                            to={"/moreComments"}
+                            style={{textDecoration: "none"}}
+                            className={classes.expandDescription}
+                        >
+                            more
+                        </Link>
+                    </BrowserRouter>
                 </Typography>
-                <Comment text={"Simple one liner"}/>
-                <Comment text={"A two liner with a lot of random random random random random random random random random random random random random random random random random random text"}/>
+                {comments.map(comment => 
+                    <Comment text={comment.text}/>    
+                )}
                 <BrowserRouter>
                     <Link 
                         to={"/moreComments"}
@@ -72,7 +139,14 @@ const post = props => {
             </CardContent>
             <Divider light variant="middle" />
             <CardContent style={{paddingTop: 8, paddingBottom: 8}}>
-                <InputBase multiline className={classes.commentInput} placeholder="Add a comment..."/>
+                <InputBase 
+                    onChange={handleCommentInputChange}
+                    onKeyPress={handleAddCommentKeyPress} 
+                    className={classes.commentInput} 
+                    placeholder="Add a comment..."
+                    value={commentInputText}
+                    multiline 
+                />
             </CardContent>
         </Card>
     );
